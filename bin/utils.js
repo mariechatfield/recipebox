f9f61b9f9a22f2e1e63b80f4c4cc7306448f9483
Marie Chatfield Rivas <marie.rivas@data.world>
2021-11-20 23:40:19 -0600

    Run prettier

--- bin/utils.js
+++ bin/utils.js
@@ -1,50 +1,63 @@
 const fs = require("fs");
 
-const OUTPUT_DIRECTORY = 'recipes'
-const AUDIT_LOG = 'auditLog.json'
-const IGNORED_IDS = 'ignoredIds.json'
+const OUTPUT_DIRECTORY = "recipes";
+const AUDIT_LOG = "auditLog.json";
+const IGNORED_IDS = "ignoredIds.json";
 
 function escapeForFilePath(originalString) {
-  return originalString
-    // Strip out apostrophes and periods
-    .replace(/['’.]/g, '')
-    // Any non-alpha character becomes an underscore
-    .replace(/[^\w]/g, '_')
-    // Strip out multiple underscores in a row
-    .replace(/_+/g, '_');
+  return (
+    originalString
+      // Strip out apostrophes and periods
+      .replace(/['’.]/g, "")
+      // Any non-alpha character becomes an underscore
+      .replace(/[^\w]/g, "_")
+      // Strip out multiple underscores in a row
+      .replace(/_+/g, "_")
+  );
 }
 
 async function getAuditDataFromRecipe(recipeFile) {
-  const filePath = `${OUTPUT_DIRECTORY}/${recipeFile}`
+  const filePath = `${OUTPUT_DIRECTORY}/${recipeFile}`;
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, { encoding: 'utf-8' }, (err, rawData) => {
+    fs.readFile(filePath, { encoding: "utf-8" }, (err, rawData) => {
       if (err) {
         reject(err);
       }
 
       try {
-        const recipeData = JSON.parse(rawData)
-        resolve({ uid: recipeData.uid, filePath: recipeFile, hash: recipeData.hash })
+        const recipeData = JSON.parse(rawData);
+        resolve({
+          uid: recipeData.uid,
+          filePath: recipeFile,
+          hash: recipeData.hash,
+        });
       } catch (e) {
-        reject(e)
+        reject(e);
       }
-    })
-  })
+    });
+  });
 }
 
 async function updateAuditLog() {
   const allRecipes = fs
     .readdirSync(OUTPUT_DIRECTORY)
-    .filter(file => file.endsWith('.json'));
+    .filter((file) => file.endsWith(".json"));
 
-  const allRecipeData = await Promise.all(allRecipes.map(getAuditDataFromRecipe))
+  const allRecipeData = await Promise.all(
+    allRecipes.map(getAuditDataFromRecipe)
+  );
 
-  const auditData = allRecipeData.reduce((memo, recipeData) => ({
-    ...memo,
-    [recipeData.uid]: recipeData
-  }), {})
+  const auditData = allRecipeData.reduce(
+    (memo, recipeData) => ({
+      ...memo,
+      [recipeData.uid]: recipeData,
+    }),
+    {}
+  );
 
-  fs.writeFileSync(AUDIT_LOG, JSON.stringify(auditData, null, 2), { encoding: 'utf-8' })
+  fs.writeFileSync(AUDIT_LOG, JSON.stringify(auditData, null, 2), {
+    encoding: "utf-8",
+  });
 }
 
 module.exports = {
@@ -52,5 +65,5 @@ module.exports = {
   IGNORED_IDS,
   OUTPUT_DIRECTORY,
   escapeForFilePath,
-  updateAuditLog
+  updateAuditLog,
 };
